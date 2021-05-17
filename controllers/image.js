0e74f609bdc7a0355235dffe9b2569670c10eea8
Siddharth Pal <originalsidd@gmail.com>
2021-05-17 20:43:02 +0530

    initial commit

---
+++ controllers/image.js
@@ -0,0 +1,30 @@
+const Clarifai =require('clarifai');
+
+const app = new Clarifai.App({
+	apiKey: '89295f027b1d4f73b89fddfc6c69e38f'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json('unable to work with api'));
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+		.increment('entries', 1)
+		.returning('entries')
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+	handleImage, 
+	handleApiCall
+}
